0d1e94eefbdf4481c23fcc9a4e4428b791ddd935
MengluShi <2574452601@qq.com>
2018-12-19 10:59:29 +0800

    add index2

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      这是index页面
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,16 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import Header from '../components/Header'
 import Message from '../pages/Message'
+import index from '../pages/index'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'Header',
-    //   component: Header
-    // },
+    {
+      path: '/index',
+      name: 'index',
+      component: index
+    },
     {
       path:'/message',
       name:'Message',
