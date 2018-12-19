e91b07fb787a682e42854aa60331f4eda665dcf9
wanghan <1556042996@qq.com>
2018-12-19 11:09:00 +0800

    add order

---
+++ src/pages/Order.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      order页面
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Order"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 // import Header from '../components/Header'
 import Message from '../pages/Message'
 import index from '../pages/index'
+import Order from '../pages/Order'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path:'/message',
       name:'Message',
       component:Message
-    }
+    },
+    {
+      path:'/order',
+      name:'order',
+      component:Order
+    },
   ]
 })
